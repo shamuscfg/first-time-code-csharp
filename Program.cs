ccb49ead7909aee87b22f87445eb00edc90ec701
shane fritsinger <shanefrit02@gmail.com>
2022-09-04 16:36:12 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+
+WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+WriteLine("M  Make your vision your view.  M");
+WriteLine("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
